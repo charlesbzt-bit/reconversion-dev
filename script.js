45dc76234a5c94e57b9e563b1e587cc66d3af5b0
Charles <charles.bouzet@gmail.com>
2026-01-02 13:39:30 +0100

    Jour 5 - Tableau de données interactif ajouté

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 // script.js — Jour 3
-function changerTexte() {
-    const section = document.querySelector("section p");
-    section.textContent = "🎉 Bravo ! Tu viens de rendre cette page interactive avec JavaScript !";
+function changerTexte1() {
+    document.getElementById("texte1").textContent = "🎉 Texte de la section 1 changé !";
+}
+
+function changerTexte2() {
+    document.getElementById("texte2").textContent = "🎉 Texte de la section 2 changé !";
+}
+
+function changerTexte3() {
+    document.getElementById("texte3").textContent = "🎉 Texte de la section 3 changé !";
+}
+function ajouterLigne() {
+    const tableau = document.getElementById("tableau");
+    const nouvelleLigne = tableau.insertRow();
+    
+    const cellule1 = nouvelleLigne.insertCell(0);
+    const cellule2 = nouvelleLigne.insertCell(1);
+    
+    cellule1.textContent = "Nouvel Item";
+    cellule2.textContent = Math.floor(Math.random() * 100);
 }
