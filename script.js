f5c2371e6c87ef18e899760bd3b9daf2bf892eef
Charles <charles.bouzet@gmail.com>
2026-01-02 13:32:58 +0100

    Jour 3 - Ajout d'interactivité JS avec un bouton

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js — Jour 3
+function changerTexte() {
+    const section = document.querySelector("section p");
+    section.textContent = "🎉 Bravo ! Tu viens de rendre cette page interactive avec JavaScript !";
+}
